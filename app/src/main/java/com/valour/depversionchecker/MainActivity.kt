941d0f65960d0c4a09c7eeda10f3c6fb0f666240
Syed Asim Ali <s.asim.ali.se@gmail.com>
2019-01-17 23:52:38 +0500

    initial commit

---
+++ app/src/main/java/com/valour/depversionchecker/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.valour.depversionchecker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
